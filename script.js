c0d90e332d48c076bbaa86b10f972a27f12cd6bf
Praaveen2000 <69044364+Praaveen2000@users.noreply.github.com>
2020-09-05 11:53:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// get the current year automatically
+
+$('#year').text(new Date().getFullYear());
+
+
+//initializing ScrollSpy
+$('.body').scrollspy({
+    target: '#mainNavbar',
+});
+
+
+//smooth scrolling
+$('#mainNavbar a').on('click',function(event){
+            if(this.has)
+})
